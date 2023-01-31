9e345f1aa9ec1a99ecb542ad6aee09c576be8a88
M. Mert Yildiran <me@mertyildiran.com>
2023-01-31 23:57:26 +0300

    :sparkles: Add tab layout to scripting modal

--- src/components/modals/ScriptingModal/ScriptingModal.tsx
+++ src/components/modals/ScriptingModal/ScriptingModal.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Box, Fade, Modal, Backdrop } from "@mui/material";
+import { Box, Fade, Modal, Backdrop, Tabs, Tab, Typography, Button } from "@mui/material";
 import closeIcon from "./assets/close.svg"
 import styles from './ScriptingModal.module.sass'
+import CodeEditor from '@uiw/react-textarea-code-editor';
 
 const modalStyle = {
   position: 'absolute',
@@ -19,12 +20,79 @@ const modalStyle = {
   paddingBottom: "15px"
 };
 
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  const [code, setCode] = React.useState(
+    `function capturedItem(item) {\n  // Your code goes here\n}`
+  );
+
+  const saveScript = () => {
+    console.log(code);
+  };
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      style={{ width: '100%' }}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children} {value}</Typography>
+          <CodeEditor
+            value={code}
+            language="js"
+            placeholder="Please enter JS code."
+            onChange={(event) => setCode(event.target.value)}
+            padding={8}
+            style={{
+              fontSize: 14,
+              backgroundColor: "#f5f5f5",
+              fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
+              margin: 10,
+            }}
+          />
+          <Button
+            variant="outlined"
+            onClick={saveScript}
+          >
+            Save Script
+          </Button>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
 interface ScriptingModalProps {
   isOpen: boolean;
   onClose: () => void;
 }
 
 export const ScriptingModal: React.FC<ScriptingModalProps> = ({ isOpen, onClose }) => {
+
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
 
   return (
     <Modal
@@ -51,6 +119,47 @@ export const ScriptingModal: React.FC<ScriptingModalProps> = ({ isOpen, onClose 
               <div style={{ display: "flex", justifyContent: "space-between" }}>
               </div>
               <div style={{ height: "100%", width: "100%" }}>
+                <Box
+                  sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: '100%' }}
+                >
+                  <Tabs
+                    orientation="vertical"
+                    variant="scrollable"
+                    value={value}
+                    onChange={handleChange}
+                    aria-label="Vertical tabs example"
+                    sx={{ borderRight: 1, borderColor: 'divider' }}
+                  >
+                    <Tab label="Item One" {...a11yProps(0)} />
+                    <Tab label="Item Two" {...a11yProps(1)} />
+                    <Tab label="Item Three" {...a11yProps(2)} />
+                    <Tab label="Item Four" {...a11yProps(3)} />
+                    <Tab label="Item Five" {...a11yProps(4)} />
+                    <Tab label="Item Six" {...a11yProps(5)} />
+                    <Tab label="Item Seven" {...a11yProps(6)} />
+                  </Tabs>
+                  <TabPanel value={value} index={0}>
+                    Item One
+                  </TabPanel>
+                  <TabPanel value={value} index={1}>
+                    Item Two
+                  </TabPanel>
+                  <TabPanel value={value} index={2}>
+                    Item Three
+                  </TabPanel>
+                  <TabPanel value={value} index={3}>
+                    Item Four
+                  </TabPanel>
+                  <TabPanel value={value} index={4}>
+                    Item Five
+                  </TabPanel>
+                  <TabPanel value={value} index={5}>
+                    Item Six
+                  </TabPanel>
+                  <TabPanel value={value} index={6}>
+                    Item Seven
+                  </TabPanel>
+                </Box>
               </div>
             </div>
           </div>
