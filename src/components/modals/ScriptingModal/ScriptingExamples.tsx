eea6e59fb9faa4b2666dc40cd909ecc9ed7b8fa6
M. Mert Yildiran <me@mertyildiran.com>
2023-02-06 05:20:33 +0300

    :sparkles: Add an example script to print constants

--- src/components/modals/ScriptingModal/ScriptingExamples.tsx
+++ src/components/modals/ScriptingModal/ScriptingExamples.tsx
@@ -18,14 +18,6 @@ function capturedItem(data) {
   if (data.Request.path === "/health")
     webhook("POST", WEBHOOK_URL, data);
 }
-
-function capturedPacket(info) {
-  // Your code goes here
-}
-
-function queriedItem(data) {
-  // Your code goes here
-}
 `;
 
 const SCRIPT_SLACK = `// Slack Example
@@ -33,14 +25,6 @@ const SCRIPT_SLACK = `// Slack Example
 function capturedItem(data) {
   if (data.Response.status === 500)
     slack(SLACK_AUTH_TOKEN, SLACK_CHANNEL_ID, "Server-side Error", JSON.stringify(data), "#ff0000");
-}
-
-function capturedPacket(info) {
-  // Your code goes here
-}
-
-function queriedItem(data) {
-  // Your code goes here
 }
 `;
 
@@ -58,24 +42,9 @@ function capturedPacket(info) {
   packetCount++
   totalBytes += info.Length
 }
-
-function queriedItem(data) {
-  // Your code goes here
-}
 `;
 
 const SCRIPT_MONITORING_PASS_HTTP = `// Monitoring: Pass HTTP Traffic, Fail Anything Else Example
-
-var packetCount = 0;
-var totalBytes = 0;
-
-function capturedItem(data) {
-  // Your code goes here
-}
-
-function capturedPacket(info) {
-  // Your code goes here
-}
 
 function queriedItem(data) {
   if (data.Protocol.Name == "http")
@@ -85,12 +54,17 @@ function queriedItem(data) {
 }
 `;
 
+const SCRIPT_PRINT_CONSTS = `// Print Constants Example
+console.log(CONSTS);
+`
+
 const EXAMPLE_SCRIPTS = [
   SCRIPT_EMPTY,
   SCRIPT_SLACK,
   SCRIPT_WEBHOOK,
   SCRIPT_PACKET_AND_BYTE_COUNTER,
   SCRIPT_MONITORING_PASS_HTTP,
+  SCRIPT_PRINT_CONSTS
 ]
 
 const EXAMPLE_SCRIPT_TITLES = [
@@ -99,6 +73,7 @@ const EXAMPLE_SCRIPT_TITLES = [
   "Call a Webhook",
   "Packet and Byte Counter",
   "Monitoring: Pass HTTP Traffic, Fail Anything Else",
+  "Print Constants",
 ]
 
 const DEFAULT_TITLE = "New Script"
