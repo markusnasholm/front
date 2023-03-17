7f5f6e8343f9ee575b6ee3b7344a80335b1e9200
M. Mert Yildiran <me@mertyildiran.com>
2023-03-17 20:14:07 +0300

    :zap: Add `title` field to node limit text

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -20,7 +20,7 @@ export const Header: React.FC<HeaderProps> = ({ licenseEdition, licenseExpired, 
       <div className="description">Traffic analyzer for Kubernetes</div>
       <div className="edition" title={licenseEdition === "community" || licenseExpired ? "" : Moment(+licenseEnd)?.utc().format('MM/DD/YYYY, h:mm:ss.SSS A')}>({licenseEdition} Edition)</div>
       {licenseExpired && <div className="extra" title={Moment(+licenseEnd)?.utc().format('MM/DD/YYYY, h:mm:ss.SSS A')}>License expired!</div>}
-      {licenseEdition !== "community" && !licenseExpired && <div className={`extra ${licenseCurrentNodeCount > licenseNodeLimit ? "failure" : "success" }`}>{`Node limit: ${licenseCurrentNodeCount}/${licenseNodeLimit}`}</div>}
+      {licenseEdition !== "community" && !licenseExpired && <div className={`extra ${licenseCurrentNodeCount > licenseNodeLimit ? "failure" : "success" }`} title={`${licenseCurrentNodeCount > licenseNodeLimit ? `You are missing traffic from ${licenseCurrentNodeCount - licenseNodeLimit} nodes!` : "OK." }`}>{`Node limit: ${licenseCurrentNodeCount}/${licenseNodeLimit}`}</div>}
     </div>
     <div style={{ display: "flex", alignItems: "center" }}>
       <UI.InformationIcon />
