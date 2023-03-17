988246b9dcfe793746f75bb98d27d274bb61e53f
M. Mert Yildiran <me@mertyildiran.com>
2023-03-17 20:11:00 +0300

    :hammer: `Viewer` to `Analyzer`

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -17,7 +17,7 @@ export const Header: React.FC<HeaderProps> = ({ licenseEdition, licenseExpired, 
     <div style={{ display: "flex", alignItems: "center" }}>
       <img className="logo" src={logo} alt="logo" />
       <div className="title">Kubeshark</div>
-      <div className="description">Traffic viewer for Kubernetes</div>
+      <div className="description">Traffic analyzer for Kubernetes</div>
       <div className="edition" title={licenseEdition === "community" || licenseExpired ? "" : Moment(+licenseEnd)?.utc().format('MM/DD/YYYY, h:mm:ss.SSS A')}>({licenseEdition} Edition)</div>
       {licenseExpired && <div className="extra" title={Moment(+licenseEnd)?.utc().format('MM/DD/YYYY, h:mm:ss.SSS A')}>License expired!</div>}
       {licenseEdition !== "community" && !licenseExpired && <div className={`extra ${licenseCurrentNodeCount > licenseNodeLimit ? "failure" : "success" }`}>{`Node limit: ${licenseCurrentNodeCount}/${licenseNodeLimit}`}</div>}
